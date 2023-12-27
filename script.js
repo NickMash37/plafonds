ef183095d8e7d1102ddd7ceadd62169f853c1e7f
Nikolay <areyfclm@gmail.com>
2023-12-27 19:59:54 +0300

    faq section almost done

---
+++ script.js
@@ -0,0 +1,11 @@
+const arrowDown = document.querySelectorAll('.fa-angle-down')
+const text = document.querySelectorAll('.text')
+const headText = document.querySelector('.head-text')
+
+// for (const arrow of arrowDown) {
+//     arrow.addEventListener('click', () => {
+//         text.forEach((item, idx) => {
+//             console.log(item[0]);
+//         })
+//     })
+// }
