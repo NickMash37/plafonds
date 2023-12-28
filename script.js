af6166b37cad647595f82d5befdf40e240601515
Nikolay <areyfclm@gmail.com>
2023-12-28 16:30:07 +0300

    faq section completely done, bug fixes

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
 const arrowDown = document.querySelectorAll('.arrow-block')
-const btn = document.querySelector('.fa-angle-down')
+const btn = document.querySelectorAll('.fa-angle-down')
 const text = document.querySelectorAll('.text')
 const headText = document.querySelectorAll('.head-text')
+const hidden = document.querySelector('.arrow-block__hidden')
+const showMoreBtn = document.querySelector('.show-more')
 
 arrowDown.forEach((el, idx) => {
     el.addEventListener('click', () => {
         el.nextElementSibling.classList.toggle('visible')
+        btn[idx].classList.toggle('transform')
     })
+})
+
+showMoreBtn.addEventListener('click', () => {
+    if (showMoreBtn.innerHTML === 'Показать больше') {
+        hidden.classList.toggle('visible')
+        showMoreBtn.innerHTML = 'Показать меньше'
+    } else {
+        hidden.classList.toggle('visible')
+        showMoreBtn.innerHTML = 'Показать больше'
+    }
 })
