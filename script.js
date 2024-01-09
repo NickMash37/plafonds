c153d424f1cf4783e1cc2e6e3ef67dface6fbb48
Nikolay <areyfclm@gmail.com>
2024-01-09 21:27:54 +0300

    mobile menu scripts

--- script.js
+++ script.js
@@ -4,6 +4,11 @@ const text = document.querySelectorAll('.text')
 const headText = document.querySelectorAll('.head-text')
 const hidden = document.querySelector('.arrow-block__hidden')
 const showMoreBtn = document.querySelector('.show-more')
+const mobileNavBar = document.querySelector('.mobile-nav__navbar')
+const burger = document.querySelector('.fa-bars')
+const burgerClose = document.querySelector('.fa-xmark')
+const mobileLinks = document.querySelectorAll('.mobile-link')
+const header = document.querySelector('.header')
 
 arrowDown.forEach((el, idx) => {
     el.addEventListener('click', () => {
@@ -20,4 +25,27 @@ showMoreBtn.addEventListener('click', () => {
         hidden.classList.toggle('visible')
         showMoreBtn.innerHTML = 'Показать больше'
     }
+})
+
+burger.addEventListener('click', () => {
+    mobileNavBar.classList.add('enabled')
+    burgerClose.classList.toggle('enabled')
+    header.classList.add('fixed')
+    document.documentElement.classList.add('no-scroll')
+})
+
+burgerClose.addEventListener('click', () => {
+    mobileNavBar.classList.remove('enabled')
+    burgerClose.classList.toggle('enabled')
+    header.classList.remove('fixed')
+    document.documentElement.classList.remove('no-scroll')
+})
+
+mobileLinks.forEach(el => {
+    el.addEventListener('click', () => {
+        mobileNavBar.classList.remove('enabled')
+        burgerClose.classList.toggle('enabled')
+        header.classList.remove('fixed')
+        document.documentElement.classList.remove('no-scroll')
+    })
 })
